59b41be07de49195d9cb17dd8f37b92e409854a7
Thedudea <140231803+Thedudea@users.noreply.github.com>
2023-07-22 09:50:21 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x252e248d8a742bff949dc9e3dbbfe21baa18c489";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
